20a15737a18fd0370a72567d4a8b506678a214cc
Peer Richelsen <peer@cal.com>
2024-05-07 21:31:31 +0200

    added github banner

--- starter/src/lib/constants.ts
+++ starter/src/lib/constants.ts
@@ -4,7 +4,7 @@ export const professions = [
   { label: "Hair dresser", value: "hair-dresser" },
   { label: "Therapist", value: "therapist" },
   { label: "Dermatologist", value: "dermatologist" },
-]  satisfies Array<Option>;
+] satisfies Array<Option>;
 
 export const services = [
   { name: "Haircut", isRemote: false, profession: "hair-dresser" },
@@ -12,8 +12,8 @@ export const services = [
   { name: "Hair styling", isRemote: false, profession: "hair-dresser" },
   { name: "Beard trimming", isRemote: true, profession: "hair-dresser" },
   { name: "Therapy", isRemote: true, profession: "therapist" },
-  { name: "Skin consultation", isRemote: false, profession: "dermatologist"},
-] satisfies Array<{name: string; isRemote: boolean, profession: string}>;
+  { name: "Skin consultation", isRemote: false, profession: "dermatologist" },
+] satisfies Array<{ name: string; isRemote: boolean; profession: string }>;
 
 export const defaultSort = {
   title: "Relevance",
@@ -31,3 +31,7 @@ export const sorting = [
     reverse: false,
   }, // asc
 ];
+
+export const IS_PRODUCTION = process.env.NODE_ENV === "production";
+export const IS_SANDBOX = process.env.NEXT_PUBLIC_CAL_API_URL === "https://api.cal.dev/v2";
+export const IS_CALCOM = process.env.VERCEL_URL === "experts.cal.com";
