c45d075976af5fb12168a501291f2b5e16189188
Richard Poelderl <richard.poelderl@gmail.com>
2024-05-13 20:03:42 +0200

    fix: make header sticky

--- starter/src/app/dashboard/layout.tsx
+++ starter/src/app/dashboard/layout.tsx
@@ -38,8 +38,8 @@ export default async function Layout({
   if (!user) return redirect("/login");
   return (
     <div className="grid min-h-screen w-full lg:grid-cols-[220px_1fr] 2xl:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
+      <div className="hidden border-r bg-muted/40 backdrop-blur md:block">
+        <div className="sticky top-0 z-40 flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
               <Package2 className="h-6 w-6" />
@@ -90,7 +90,7 @@ export default async function Layout({
         </div>
       </div>
       <div className="flex flex-col">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+        <header className="sticky top-0 z-40 flex h-14 items-center gap-4 border-b bg-muted/40 px-4 backdrop-blur lg:h-[60px] lg:px-6">
           <Sheet>
             <SheetTrigger asChild>
               <Button size="icon" variant="outline" className="sm:hidden">
