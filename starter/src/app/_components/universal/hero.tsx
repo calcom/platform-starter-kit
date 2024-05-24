e126847222a9a0d51a87d39f35700d958a2959bb
Olga Stogova <helgastogova@gmail.com>
2024-05-24 13:08:24 +0200

    fix for the horizontal scroll for the hero component

--- starter/src/app/_components/universal/hero.tsx
+++ starter/src/app/_components/universal/hero.tsx
@@ -8,7 +8,7 @@ interface HeroProps {
 export const Hero = ({ title, children }: HeroProps) => {
   return (
     <div
-      className="flex min-h-96 w-screen flex-col justify-center bg-cover bg-center bg-no-repeat py-20"
+      className="flex min-h-96 flex-col justify-center bg-cover bg-center bg-no-repeat py-20"
       style={{ backgroundImage: "url('/hero.jpg')" }}>
       <div className="container mt-16 flex flex-col items-center justify-center gap-12 px-4 py-6">
         <h1 className="font-display text-5xl font-extrabold tracking-tight text-white">
