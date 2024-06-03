16d255054d64dac4e5a2c5688c588d8285d0f42b
Richard Poelderl <richard.poelderl@gmail.com>
2024-06-03 23:25:11 +0200

    add `bio` to expert details page

--- starter/src/app/experts/[expertUsername]/page.tsx
+++ starter/src/app/experts/[expertUsername]/page.tsx
@@ -1,66 +1,15 @@
 import { ExpertBooker } from "./_components/expert-booker";
 import { Badge } from "@/components/ui/badge";
+import { CardDescription } from "@/components/ui/card";
+import { type User } from "@prisma/client";
 import Image from "next/image";
 import { db } from "prisma/client";
-
-interface Item {
-  name: string;
-}
-
-interface ExpertDetailsSectionProps {
-  title: string;
-  items: Item[];
-  itemLimit: number;
-}
-
-interface Profession {
-  name: string;
-}
-
-interface Expert {
-  name: string;
-  professions: Profession[];
-}
-
-function ExpertHeader({ expert }: { expert: Expert }) {
-  return (
-    <div className="flex items-center gap-x-6">
-      <Image
-        alt="Expert image"
-        className="aspect-square rounded-md object-cover"
-        src="https://picsum.photos/200"
-        height="64"
-        width="64"
-      />
-      <div>
-        <div className="text-sm leading-6 text-muted-foreground">
-          {expert.professions.map((profession) => profession.name).join(", ")}
-        </div>
-        <h1 className="text-2xl font-semibold leading-none tracking-tight">{expert.name}</h1>
-      </div>
-    </div>
-  );
-}
-
-function ExpertDetailsSection({ title, items, itemLimit }: ExpertDetailsSectionProps) {
-  return (
-    <div className="flex flex-col space-y-1.5 p-0 lg:p-6">
-      <div className="text-sm leading-6 text-muted-foreground">{title}</div>
-      <div className="flex gap-1">
-        {items.slice(0, itemLimit).map(({ name }, idx) => (
-          <Badge key={name}>{name}</Badge>
-        ))}
-        {items.length > itemLimit && <Badge>+{items.length - itemLimit} more</Badge>}
-      </div>
-    </div>
-  );
-}
 
 export default async function ExpertDetails({ params }: { params: { expertUsername: string } }) {
   console.log("expertUsername: ", params.expertUsername);
   const expert = await db.user.findUnique({
     where: { username: params.expertUsername },
-    include: { calAccount: true, services: true, professions: true },
+    include: { calAccount: true },
   });
   if (!expert) {
     console.warn("Expert not found", params.expertUsername);
@@ -70,11 +19,30 @@ export default async function ExpertDetails({ params }: { params: { expertUserna
   return (
     <div className="flex flex-1 flex-col items-center gap-4 overflow-auto">
       <div className="flex w-full flex-col justify-between gap-4 rounded-md bg-muted/50 px-8 py-4  sm:px-10 lg:flex-row lg:px-12">
-        <ExpertHeader expert={expert} />
-        <div className="flex items-center gap-x-4 sm:gap-x-6">
-          <ExpertDetailsSection title="Professions" items={expert.professions} itemLimit={2} />
-          <ExpertDetailsSection title="Services" items={expert.services} itemLimit={2} />
+        <div className="flex items-center gap-x-6">
+          <Image
+            alt="Expert image"
+            className="aspect-square rounded-md object-cover"
+            src="https://picsum.photos/200"
+            height="64"
+            width="64"
+          />
+          <div>
+            <h1 className="text-2xl font-semibold capitalize leading-none tracking-tight">{expert.name}</h1>
+          </div>
         </div>
+        <div className="flex items-center gap-x-4 sm:gap-x-6">
+          {/* <ExpertDetailsSection title="Professions" items={expert.professions} itemLimit={2} />
+          <ExpertDetailsSection title="Services" items={expert.services} itemLimit={2} /> */}
+        </div>
+      </div>
+      <div className="mx-auto mt-4 grid w-full gap-2 px-8 sm:px-10 lg:px-12">
+        <h2 className="text-3xl font-semibold">About Us</h2>
+      </div>
+      <div className="mx-auto mt-4 grid w-full gap-2 px-8 sm:px-10 lg:px-12">
+        <p className="max-w-lg text-balance text-sm leading-relaxed text-muted-foreground">
+          Introducing Our Dynamic Orders Dashboard for Seamless Management and Insightful Analysis.
+        </p>
       </div>
       <div className="mx-auto mt-4 grid w-full gap-2 px-8 sm:px-10 lg:px-12">
         <h2 className="text-3xl font-semibold">Availability</h2>
